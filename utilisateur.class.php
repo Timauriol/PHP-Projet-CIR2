ad1c6fa1e93cee5a0bb5e9f46d6e9c3b954b040e
Corentin Delcourt <codl@codl.fr>
2013-05-21 09:49:10 +0200

    ajouté Utilisateur::tous() et ::recherche($q)

--- utilisateur.class.php
+++ utilisateur.class.php
@@ -25,17 +25,47 @@ class Utilisateur{
         else{
             $this->login = $login;
             $this->nom_prenom = $nom_prenom;
-            $this->admin = $admin;
+            $this->admin = $admin == "1";
         }
     }
 
     public static function tous(){
         // renvoie un Array de tous les utilisateurs
-        // TODO
+        global $db;
+        $req = $db->prepare("SELECT login, nom_prenom, admin FROM utilisateur");
+        if(!$req->execute()) die("Problème de connexion à la base MySQL");
+        $tous = array();
+        while($res = $req->fetch()){
+            array_push($tous, new Utilisateur($res[0], $res[1], $res[2]));
+        }
+        return $tous;
     }
     public static function recherche($q){
         // renvoie un Array des utilisateurs correspondant à la recherche
-        // TODO
+        global $db;
+        $query = "SELECT login, nom_prenom, admin FROM utilisateur WHERE 1=1";
+        $args = explode(" ", $q);
+
+        for($i=0; $i < count($args); $i++)
+            $query .= " AND ( login COLLATE utf8_general_ci LIKE ? OR nom_prenom COLLATE utf8_general_ci LIKE ? )";
+
+        $req = $db->prepare($query);
+
+        for($i=0; $i < count($args); $i++){
+            $arg = "%".$args[$i]."%";
+            $req->bindParam($i*2+1, $arg);
+            $req->bindParam($i*2+2, $arg);
+        }
+
+
+        $res = $req->execute();
+
+
+        $utilisateurs = array();
+        while($res = $req->fetch()){
+            array_push($utilisateurs, new Utilisateur($res[0], $res[1], $res[2]));
+        }
+        return $utilisateurs;
     }
 }
 ?>
